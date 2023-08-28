0de7dc945602a15340924db0d0ebd00c0c00c5c4
Oscar Santos <oscar303data@gmail.com>
2023-08-28 12:49:48 -0600

    create pages and components folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-
+    <div className='App'>
+      
+      <div className='pages'>
+        <Routes>
+          <Route path='/' element={<h1>Home</h1>} />
+        </Routes>
+      </div>
     </div>
   );
 }
