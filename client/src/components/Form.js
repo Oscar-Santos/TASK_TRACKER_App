b37736ae68a52e48e331f56d72e217b5a5bb2bd6
Oscar Santos <oscar303data@gmail.com>
2023-09-01 14:01:16 -0600

    add select tags and functionality to have 3 options for priority and status of the App

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,64 +1,88 @@
-import { useState } from "react"
 
+import React, { useState } from "react";
 
 export default function Form() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("");
+  const [status, setStatus] = useState("");
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
 
-    const [title, setTitle] = useState('')
-    const [description, setDescription] = useState('')
-    const [priority, setPriority] = useState('')
-    const [status, setStatus] = useState('')
-    const [error, setError] = useState(null)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-      e.preventDefault()
-      
-      const task = { title, description, priority, status }
+    const task = { title, description, priority, status };
 
-      const response = await fetch('api/tasks', {
-            method: 'POST',
-            body: JSON.stringify(task),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-      })
-
-      const json = await response.json()
+    try {
+      const response = await fetch("/api/tasks", {
+        method: "POST",
+        body: JSON.stringify(task),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
       if (!response.ok) {
-        setError(json.error)
-        // setError(json.message)
+        const json = await response.json();
+        setError(json.error || "Something went wrong");
+        setSuccess(null);
+      } else {
+        setTitle("");
+        setDescription("");
+        setPriority("");
+        setStatus("");
+        setError(null);
+        setSuccess("Task added successfully");
       }
-      if (response.ok) {
-        setTitle("")
-        setDescription("")
-        setPriority("")
-        setStatus("")
-        setError(null)
-        console.log('new task added successfully', json)
-      }
+    } catch (error) {
+      setError("Something went wrong");
+      setSuccess(null);
     }
-   
-    return (
-        <div>
-            <form action="" className="create" onSubmit={handleSubmit}>
-                <h3>Add a new Task</h3>
+  };
 
-                <label> Title:</label>
-                <input type="text" onChange={(e) => setTitle(e.target.value)} value={title}/>
-                
+  return (
+    <div>
+      <form action="" className="create" onSubmit={handleSubmit}>
+        <h3>Add a new Task</h3>
 
-                <label> Description:</label>
-                <input type="text" onChange={(e) => setDescription(e.target.value)} value={description}/>
-                
-                <label> Priority:</label>
-                <input type="text" onChange={(e) => setPriority(e.target.value)} value={priority}/>
-                
+        <label>Title:</label>
+        <input
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+          required
+        />
 
-                <label> Status:</label>
-                <input type="text" onChange={(e) => setStatus(e.target.value)} value={status}/>
+        <label>Description:</label>
+        <textarea
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
+          required
+        />
 
-                <button>Add Task</button>
-            </form>
-        </div>
-    )
+        <label>Priority:</label>
+        <select onChange={(e) => setPriority(e.target.value)} value={priority}>
+          <option value="">Select Priority</option>
+          <option value="low">Low</option>
+          <option value="medium">Medium</option>
+          <option value="high">High</option>
+        </select>
+
+        <label>Status:</label>
+        <select onChange={(e) => setStatus(e.target.value)} value={status}>
+          <option value="">Select Status</option>
+          <option value="todo">To Do</option>
+          <option value="inProgress">In Progress</option>
+          <option value="completed">Completed</option>
+        </select>
+
+        <button>Add Task</button>
+      </form>
+
+      {/* Error and Success Messages */}
+      {error && <p className="error">{error}</p>}
+      {success && <p className="success">{success}</p>}
+    </div>
+  );
 }
