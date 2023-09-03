5e2b912eb244b690b04df9418f1eac40cad02c4e
Oscar Santos <oscar303data@gmail.com>
2023-09-03 17:18:18 -0600

    it can update the title task

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -7,6 +7,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+
   const navigate = useNavigate()
   const {tasks, dispatch} = useTasksContext()
 
@@ -29,7 +30,51 @@ const Home = () => {
     fetchTasks()
   }, [])
 
+//1
+const [updatedTask, setUpdatedTask] = useState({
+  _id: '', // Store the task ID for updates
+  title: '',
+  description: '',
+  priority: '',
+  status: '',
+});
 
+// 2
+const [isUpdateFormVisible, setIsUpdateFormVisible] = useState(false);
+const [successMessage, setSuccessMessage] = useState(null);
+
+
+const handleUpdateSubmit = async () => {
+  try {
+    const response = await fetch(`/api/tasks/${updatedTask._id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedTask),
+    });
+
+    if (response.ok) {
+      // Update the local state with the updated task
+      const updatedTaskIndex = tasks.findIndex((task) => task._id === updatedTask._id);
+      const updatedTasks = [...tasks];
+      updatedTasks[updatedTaskIndex] = updatedTask;
+      dispatch({ type: 'SET_TASKS', payload: updatedTasks });
+
+      setSuccessMessage('Task updated successfully');
+      setIsUpdateFormVisible(false);
+
+      setTimeout(() => {
+        setSuccessMessage(null);
+      }, 3000);
+
+    } else {
+      console.error('Failed to update task');
+    }
+  } catch (error) {
+    console.error('Error updating task:', error);
+  }
+};
 
   const handleClick = async (taskId) => {
    console.log(taskId)
@@ -54,33 +99,95 @@ const Home = () => {
 
 
   return (
-    <div className='home'>
-      <div className='tasks'>
-        {tasks && tasks.map((task) => (
+  //   <div className='home'>
+  //     <div className='tasks'>
+  //       {tasks && tasks.map((task) => (
       
-      <div className='task-details' key={task._id}>
-        <h4>Task</h4>
-          <p key={task._id}><strong>Title:</strong> {task.title}</p>
+  //     <div className='task-details' key={task._id}>
+  //       <h4>Task</h4>
+  //         <p key={task._id}><strong>Title:</strong> {task.title}</p>
           
-          <p key={task._id}> <strong>Description:</strong> {task.description}</p>
+  //         <p key={task._id}> <strong>Description:</strong> {task.description}</p>
           
-          <p key={task._id}><strong>Priority</strong> {task.priority}</p>
+  //         <p key={task._id}><strong>Priority</strong> {task.priority}</p>
           
-          <p key={task._id}><strong>Status:</strong> {task.status}</p> 
+  //         <p key={task._id}><strong>Status:</strong> {task.status}</p> 
       
-          <p key={task._id}><strong>Created:</strong> {task.createdAt}</p> 
+  //         <p key={task._id}><strong>Created:</strong> {task.createdAt}</p> 
           
-          <span className="delete-button" onClick={() => handleClick(task._id)}>Delete</span>
+  //         <span className="delete-button" onClick={() => handleClick(task._id)}>Delete</span>
              
-          <span className="update-button"> Update</span>
+  //         <span className="update-button"> Update</span>
         
-   </div>
-          
-        ))}
+  //  </div>
+     
+  //3
+  <div className='home'>
+  <div className='tasks'>
+    {tasks &&
+      tasks.map((task) => (
+        <div className='task-details' key={task._id}>
+          <h4>Task</h4>
+          <p>
+            <strong>Title:</strong> {task.title}
+          </p>
+
+          <p>
+            <strong>Description:</strong> {task.description}
+          </p>
+
+          <p>
+            <strong>Priority</strong> {task.priority}
+          </p>
+
+          <p>
+            <strong>Status:</strong> {task.status}
+          </p>
+
+          <p>
+            <strong>Created:</strong> {task.createdAt}
+          </p>
+
+          {isUpdateFormVisible && task._id === updatedTask._id ? (
+            <div className='update-form'>
+              <input
+                type='text'
+                placeholder='Updated Title'
+                value={updatedTask.title}
+                onChange={(e) =>
+                  setUpdatedTask({ ...updatedTask, title: e.target.value })
+                }
+              />
+              {/* Repeat this for other task attributes (description, priority, status) */}
+              <button onClick={handleUpdateSubmit}>Update Task</button>
+            </div>
+          ) : (
+            <>
+              <span
+                className='delete-button'
+                onClick={() => handleClick(task._id)}
+              >
+                Delete
+              </span>
+              <span
+                className='update-button'
+                onClick={() => {
+                  setUpdatedTask(task); // Set the task data to be updated
+                  setIsUpdateFormVisible(true); // Show the update form
+                }}
+              >
+                Update
+              </span>
+            </>
+          )}
+        </div>
+        )
+        
+  )}
     
       </div>
          <Form /> 
-         
+         {successMessage && <p className="success">{successMessage}</p>}
     </div>
   )
   }
