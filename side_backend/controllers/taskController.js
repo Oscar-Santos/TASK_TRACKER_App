5dde6024e12810324ab9000fa5d9e3efafb7df25
Oscar Santos <oscar303data@gmail.com>
2023-09-04 05:09:09 -0600

    make a backend copy

---
+++ side_backend/controllers/taskController.js
@@ -0,0 +1,78 @@
+const Task = require('../models/taskModel');
+const mongoose = require('mongoose');
+
+
+
+// get all tasks
+const getTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find()
+        res.status(200).json(tasks);
+    } catch(error) {
+        res.status(404).json({message: 'cannot get all tasks'})
+    }
+}
+
+// get a specific task
+const getTask = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({message: 'cannot get a specific task'})
+    }
+    try {
+        const task = await Task.findById(id)
+        res.status(200).json(task)
+    } catch(error) {
+        res.status(404).json({message: 'cannot get a specific task'})
+    }
+}
+
+const deleteTask = async (req, res) => {
+    const { id } = req.params
+  
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({error: 'No such workout'})
+    }
+  
+    const task = await Task.findByIdAndDelete({_id: id})
+  
+    if(!task) {
+      return res.status(400).json({error: 'No such workout'})
+    }
+  
+    res.status(200).json(task)
+  }
+// create a new task
+const createTask = async (req, res) => {
+    const { title, description, priority, status } = req.body
+
+    try {
+        const task = await Task.create( { title, description, priority, status } )
+        res.status(200).send(task)
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+        // res.status(400).json({ message: 'cannot create a task' })
+    }
+    
+}
+
+// update a specific task 
+const updateTaskAttr = async (req, res) => {
+    const { id } = req.params
+    try {
+        const task = await Task.findOneAndUpdate({_id: id}, {...req.body})
+        res.status(200).json(task)
+    } catch(error) {
+        res.status(400).json({ message: 'cannot update a task attribute' })
+    }
+}
+
+module.exports = {
+    createTask,
+    getTasks,
+    getTask,
+    deleteTask,
+    updateTaskAttr
+}
+
