d34e5a6b71fd4c36a56cf41d35404cc7d0838a9e
Oscar Santos <oscar303data@gmail.com>
2023-09-04 16:42:48 -0600

    set up Auth context

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,17 @@ import './index.css'
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { TasksContextProvider } from './context/taskContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <Router>
-    <TasksContextProvider>
-    <App />
-    </TasksContextProvider>
+    <AuthContextProvider>
+      <TasksContextProvider>
+        <App />
+      </TasksContextProvider>
+    </AuthContextProvider>
   </Router> 
   
 
