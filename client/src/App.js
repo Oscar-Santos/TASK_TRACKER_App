08861cf016e338243a75682b55d4554fe37091a8
Oscar Santos <oscar303data@gmail.com>
2023-09-05 12:01:55 -0600

    add navigate, to redirect all pages to the login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuthContext';
+
 import './App.css'
 import Home from './pages/Home';
 import About from './pages/About';
-import Documentation from './pages/Documentation';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
 import Nav from './components/Nav';
 
 function App() {
+  const { user } = useAuthContext();
+
   return (
     <div className='app'>
      
       
         <Nav />
         <Routes>       
-          <Route path='/about' element={<About />} />
-          <Route path='/home' element={<Home/>} />
-          <Route path='/documentation' element={<Documentation />} />
+          <Route path='/about' element={user ? <About/>  : <Navigate to='/login'/>} />
+          <Route path='/home' element={user ? <Home/> : <Navigate to='/login'/>} />
 
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={!user ? <Login /> : <Navigate to='/'/>} />
+          <Route path='/signup' element={!user ? <Signup /> : <Navigate to='/'/>} />
         </Routes>
   
     </div>
