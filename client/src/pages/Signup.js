8cadd84eb40bdbf1ed45905cb8f2b14eafaf691e
Oscar Santos <oscar303data@gmail.com>
2023-09-04 20:10:06 -0600

    set up hooks useSignup and useAuthContext

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,13 +1,13 @@
 import { useState } from 'react';
-
+import { useSignup } from '../hooks/useSignup';
 const Signup = () => {
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
-
+    const { signup, error, isLoading } = useSignup();
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        console.log(email, password)
+        await signup(email, password)
     }
 
     return (
@@ -29,7 +29,9 @@ const Signup = () => {
             onChange={(e) => setPassword(e.target.value)}
             value={password}
             />
-            <button className='signup-button'>Sign up</button>
+            <button className='signup-button' disabled={isLoading}>Sign up</button>
+            {error && <div className='error'>{error}</div>}
+            
         </form>
     )
 }
