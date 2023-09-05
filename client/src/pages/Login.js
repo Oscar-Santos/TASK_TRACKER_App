1df0bfe0f0d43cf3b04dba5c487b823521e563f8
Oscar Santos <oscar303data@gmail.com>
2023-09-04 20:53:30 -0600

    set up the login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,13 +1,15 @@
 import { useState } from 'react';
+import { useLogin } from '../hooks/useLogin';
 
 const Login = () => {
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
+    const { login, error, isLoading } = useLogin();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        console.log(email, password)
+        await login(email, password)
     }
 
     return (
@@ -30,7 +32,9 @@ const Login = () => {
             value={password}
             />
 
-            <button className='login-button'>Log in</button>
+            <button className='login-button' disabled={isLoading}>Log in</button>
+            {error && <div className='error'>{error}</div>}
+            
         </form>
     )
 }
