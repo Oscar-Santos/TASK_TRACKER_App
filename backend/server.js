b910f820eb0c0e2ebb86af6b9b7aeafd7f5ef0ca
Oscar Santos <oscar303data@gmail.com>
2023-11-09 17:29:44 -0700

    add CORS

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const cors = require('cors');
 const express = require('express');
 const moongoose = require('mongoose');
 const app = express();
@@ -11,7 +11,7 @@ const userRoutes = require('./routes/user');
 // midleware
 app.use(express.json());
 
-
+app.use(cors());
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next();
